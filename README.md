d800bffb0ee2d14935cb16806deb001b28eb588d
Nazar Vynnytskyi <nazar.vynn@gmail.com>
2016-08-24 13:12:51 +0300

    Added commands (add, show) to MongoDB

--- README.md
+++ README.md
@@ -20,11 +20,15 @@ $sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 $echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
 $sudo apt-get update
 
+//Global installation
 //x32
 $sudo apt-get install -y mongodb
 
 //x64
 $sudo apt-get install -y mongodb-org
+
+//Local installation
+$npm install mongodb --save
 
 ```
 
--- app.js
+++ app.js
@@ -2,10 +2,20 @@ var MongoClient = require('mongodb').MongoClient;
 var format = require('util').format;
 
 MongoClient.connect('mongodb://127.0.0.1:27017/test', function (err, db) {
-    if (err) {
-        throw err;
-    } else {
-        console.log("successfully connected to the database");
-    }
-    db.close();
+    if (err) throw err;
+
+    console.log("successfully connected to the database");
+
+    var collection = db.collection('test_insert');
+
+    collection.insert({ a: 2 }, function (error, docs) {
+        collection.count(function (error, count) {
+            console.log(format("count = %s", count));
+        });
+    });
+
+    collection.find().toArray(function(error, results) {
+        console.dir(results);
+        db.close();
+    });
 });
